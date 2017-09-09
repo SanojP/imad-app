bfdb5164f8baffdc7c29e40549355e2fe5841375
SanojP <31256083+SanojP@users.noreply.github.com>
2017-09-09 17:18:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,78 +13,8 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-var articles = { 
-'article-one': {
-  title: 'Article One | SanojP',
-  heading: 'Article One',
-  content:` <p>
-                The club isn't the best place to find a lover
-                So the bar is where I go
-                Me and my friends at the table doing shots
-                Drinking fast and then we talk slow
-                Come over and start up a conversation with just me
-                And trust me I'll give it a chance now
-                Take my hand, stop, put Van the Man on the jukebox
-                And then we start to dance, and now I'm singing like
-            </p>
-            <p>
-                Girl, you know I want your love
-                Your love was handmade for somebody like me
-                Come on now, follow my lead
-                I may be crazy, don't mind me
-                Say, boy, let's not talk too much
-                Grab on my waist and put that body on me
-                Come on now, follow my lead
-                Come, come on now, follow my lead
-  `
-},
-'article-2':  {
-    title: 'Article Two | SanojP',
-  heading: 'Article Two',
-  content:` <p>
-                The college isn't the best place to find a lover
-                So the bar is where I go
-                Me and my friends at the table doing shots
-                Drinking fast and then we talk slow
-                Come over and start up a conversation with just me
-                And trust me I'll give it a chance now
-                Take my hand, stop, put Van the Man on the jukebox
-                And then we start to dance, and now I'm singing like
-            </p>
-            <p>
-                Girl, you know I want your love
-                Your love was handmade for somebody like me
-                Come on now, follow my lead
-                I may be crazy, don't mind me
-                Say, boy, let's not talk too much
-                Grab on my waist and put that body on me
-                Come on now, follow my lead
-                Come, come on now, follow my lead
-  `},
-'article-3':  {
-    title: 'Article Three | SanojP',
-  heading: 'Article Three',
-  content:` <p>
-                The school isn't the best place to find a lover
-                So the bar is where I go
-                Me and my friends at the table doing shots
-                Drinking fast and then we talk slow
-                Come over and start up a conversation with just me
-                And trust me I'll give it a chance now
-                Take my hand, stop, put Van the Man on the jukebox
-                And then we start to dance, and now I'm singing like
-            </p>
-            <p>
-                Girl, you know I want your love
-                Your love was handmade for somebody like me
-                Come on now, follow my lead
-                I may be crazy, don't mind me
-                Say, boy, let's not talk too much
-                Grab on my waist and put that body on me
-                Come on now, follow my lead
-                Come, come on now, follow my lead
-  `}
-};
+
+   
 function createTemplate (data) {
 var title = data.title;
 var heading = data.heading;
